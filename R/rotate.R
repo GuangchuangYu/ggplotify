aea4f1c087a1c9397e936f8b244e126e37b4ce4f
Guangchuang Yu <guangchuangyu@gmail.com>
2020-03-16 20:42:41 +0800

    hjust and vjust parameters

--- R/rotate.R
+++ R/rotate.R
@@ -5,14 +5,18 @@
 ##' @param p plot that supported by ggplotify to convert to ggplot object
 ##' @param angle user selected angle
 ##' @param scale amount to scale the plot (zoom in or zoom out)
+##' @param hjust horizontal adjustment
+##' @param vjust vertical adjustment
 ##' @return display figure with specific angle
 ##' @importFrom grid viewport
 ##' @export
 ##' @author Guangchuang Yu
-rotate <- function(p, angle, scale = .9) {
+rotate <- function(p, angle, scale = .9, hjust = 0, vjust = 0) {
     g <- grid::grid.grabExpr(print(as.ggplot(p),
                                    newpage = TRUE,
-                                   vp = viewport(angle = angle,
+                                   vp = viewport(x = .5 + hjust,
+                                                 y = .5 + vjust,
+                                                 angle = angle,
                                                  width = scale,
                                                  height = scale)
                                    ))
